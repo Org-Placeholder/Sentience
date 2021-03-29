a6aaef12519f64d4371981b01fb67a120d50c97f
Angad Kambli <angadkambli@gmail.com>
2021-03-29 13:51:33 +0530

    add effect for hit

--- utility_scenes/bullet.gd
+++ utility_scenes/bullet.gd
@@ -4,6 +4,7 @@ var direction = Vector2()
 var shot = false
 const SPEED = 30
 var elapsed_time = 0
+var bullet = true
 func shoot(p_direction):
 	direction = p_direction
 	shot = true
@@ -15,4 +16,14 @@ func _physics_process(delta):
 		elapsed_time += delta
 		if(elapsed_time > 2):
 			queue_free()
-		
+
+func _on_Area2D_body_entered(body):
+	if(!body.get("bullet")):
+		shot = false
+		$Sprite.visible = false
+		$EffekseerEmitter2D.play()
+		if(body.has_method("on_hit")):
+			body.on_hit()
+		yield(get_tree().create_timer(2), "timeout")
+		queue_free()
+	pass
