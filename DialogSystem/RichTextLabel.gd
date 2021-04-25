b3700bd32b6d8be9a8c780bab45daee541d77c35
ayushgupta138 <ayush.gupta02071@gmail.com>
2021-04-25 18:30:56 +0530

    Add character by character transition on mouseclick

--- DialogSystem/RichTextLabel.gd
+++ DialogSystem/RichTextLabel.gd
@@ -10,12 +10,17 @@ var index=0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_bbcode(dialogue[index])
-	
+	set_visible_characters(0)
+	set_process_input(true)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if event is InputEventMouseButton && event.is_pressed():
+		if get_visible_characters() > get_total_character_count():
+			if index < dialogue.size() - 1 :
+				print(index)
+				index=index+1
+				set_bbcode(dialogue[index])
+				set_visible_characters(0) 
 
-
-#func _on_Timer_timeout():
-	
+func _on_Timer_timeout():
+	set_visible_characters(get_visible_characters()+1)	
