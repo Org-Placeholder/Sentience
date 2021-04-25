94aceee59d46b9d48720254967b72b858a8e415e
ayushgupta138 <ayush.gupta02071@gmail.com>
2021-04-25 18:48:38 +0530

    Add some features and skip button for dialogue system

--- DialogSystem/RichTextLabel.gd
+++ DialogSystem/RichTextLabel.gd
@@ -4,7 +4,7 @@ extends RichTextLabel
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var dialogue=["Hello ! Welcome to the game.","Added second dialogue to check dialogue transition"]
+var dialogue=["Hello ! Welcome to the game.","Added second dialogue to check dialogue transition.","Hope you enjoy the game!!"]
 var index=0
 
 # Called when the node enters the scene tree for the first time.
@@ -17,10 +17,18 @@ func _input(event):
 	if event is InputEventMouseButton && event.is_pressed():
 		if get_visible_characters() > get_total_character_count():
 			if index < dialogue.size() - 1 :
-				print(index)
 				index=index+1
 				set_bbcode(dialogue[index])
-				set_visible_characters(0) 
+				set_visible_characters(0)
+		else:
+			set_visible_characters(get_total_character_count()) 
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters()+1)	
+
+
+func _on_Button_pressed():
+	if index < dialogue.size()-1:
+		index=dialogue.size()-1;
+		set_bbcode(dialogue[index])
+		set_visible_characters(0)
