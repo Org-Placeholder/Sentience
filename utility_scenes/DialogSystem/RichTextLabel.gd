48a6b0884f3c073adf1712514c1165c7dc47ddbb
Angad Kambli <angadkambli@gmail.com>
2021-04-26 16:45:26 +0530

    dialogue system fixes

--- utility_scenes/DialogSystem/RichTextLabel.gd
+++ utility_scenes/DialogSystem/RichTextLabel.gd
@@ -6,13 +6,17 @@ extends RichTextLabel
 # var b = "text"
 var dialogue=["Hello ! Welcome to the game.","Added second dialogue to check dialogue transition.","Hope you enjoy the game!!"]
 var index=0
-
+var freed = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+
+func _initialize(p_dialogue):
+	dialogue = p_dialogue
 	set_bbcode(dialogue[index])
 	set_visible_characters(0)
 	set_process_input(true)
-
+	
 func _input(event):
 	if event is InputEventMouseButton && event.is_pressed():
 		if get_visible_characters() > get_total_character_count():
@@ -20,15 +24,20 @@ func _input(event):
 				index=index+1
 				set_bbcode(dialogue[index])
 				set_visible_characters(0)
+			else :
+				freed = true
 		else:
 			set_visible_characters(get_total_character_count()) 
+
+func _process(delta):
+	if(freed):
+		get_parent().modulate.a = lerp(get_parent().modulate.a, 0, 0.05)
+		if(is_equal_approx(get_parent().modulate.a, 0)):
+			get_parent().queue_free()
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters()+1)	
 
 
 func _on_Button_pressed():
-	if index < dialogue.size()-1:
-		index=dialogue.size()-1;
-		set_bbcode(dialogue[index])
-		set_visible_characters(0)
+	freed = true
