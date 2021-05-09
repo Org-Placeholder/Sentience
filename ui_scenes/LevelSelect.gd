48495443f6001b8045cf0b31e19bc9890348e72f
Angad Kambli <angadkambli@gmail.com>
2021-05-09 21:12:15 +0530

    fix minor issue

--- ui_scenes/LevelSelect.gd
+++ ui_scenes/LevelSelect.gd
@@ -14,30 +14,36 @@ func _ready():
 		
 	var levels = $LevelSelect.get_children()
 	var level_num = DataManager.get_last_completed_level()
-	for i in range(level_num - 1):
+	for i in range(level_num):
 		levels[i].disabled = false
 
 
 func _on_BackButton_pressed():
+	
 	get_tree().change_scene("res://ui_scenes/title_screen.tscn")
 
 
 func _on_1_pressed():
+	GameState.level_num = 1
 	get_tree().change_scene("res://gameplay_scenes/levels/level1.tscn")
 
 
 func _on_2_pressed():
+	GameState.level_num = 2
 	get_tree().change_scene("res://gameplay_scenes/levels/level2.tscn")
 
 
 func _on_3_pressed():
+	GameState.level_num = 3
 	get_tree().change_scene("res://gameplay_scenes/levels/level3.tscn") # Replace with function body.
 
 
 func _on_4_pressed():
+	GameState.level_num = 4
 	get_tree().change_scene("res://gameplay_scenes/levels/level4.tscn")
  # Replace with function body.
 
 func _on_5_pressed():
+	GameState.level_num = 5
 	get_tree().change_scene("res://gameplay_scenes/levels/level5.tscn")
  # Replace with function body.
