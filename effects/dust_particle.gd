174b0d01d83ccec24fd799aa51d3a0d98992537f
ayushgupta138 <ayush.gupta02071@gmail.com>
2021-05-14 16:02:56 +0530

    Use inbuilt particle system

---
+++ effects/dust_particle.gd
@@ -0,0 +1,18 @@
+extends Particles2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	yield(get_tree().create_timer(1),"timeout")
+	queue_free()
+	 # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
